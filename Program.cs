b09b7cbb7c0c1856ac02ad97c651673f10a6091b
Алияр Молдабеков <aliyar.m03@gmail.com>
2021-11-18 21:36:00 +0700

    Доделал лабу

--- Program.cs
+++ Program.cs
@@ -8,17 +8,36 @@ namespace LB4_IT
         {
             int[,] matrix = new int[5, 6];
             Random ran = new Random();
+
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 6; j++)
                 {
-                    matrix[i, j] = ran.Next(-100, 100);
+                    matrix[i, j] = ran.Next(0, 100);
                     Console.Write("{0}\t", matrix[i, j]);
                 }
                 Console.WriteLine();
             }
-
-            //{ { 0, 1, 2, 3, 4 }, { 3, 4, 5, 9, 17 }, { 8, 9, 17, 12, 34 }, { 0, -2, -6, 15, -28 }, { -5, 8, 10, 0, 9 }, { 7, 21, 27, 3, 4 } };
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        int mod = matrix[i, j] % (j + 1);
+                        if (mod != 0)
+                        {
+                            matrix[i, j] = mod;
+                            count++;
+                        }
+                        Console.Write("{0}\t", matrix[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Количество изменённых элементов: " + count);
         }
     }
 }
