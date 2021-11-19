3ded6cb2c02b826a91a7697cf348a31e6e995209
Алияр Молдабеков <aliyar.m03@gmail.com>
2021-11-19 14:01:27 +0700

    Закомментировал

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ namespace LB4_IT
         {
             int[,] matrix = new int[5, 6];
             Random ran = new();
+            // создание массива с размерномтью [5, 6]
 
             for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < 6; j++) 
                 {
-                    matrix[i, j] = ran.Next(0, 100);
-                    Console.Write("{0}\t", matrix[i, j]);
+                    matrix[i, j] = ran.Next(0, 100); // заполнение массива случайными числами
+                    Console.Write("{0}\t", matrix[i, j]); // вывод элемента массива с индексом [i, j] с табуляцией
                 }
                 Console.WriteLine();
             }
@@ -25,13 +26,13 @@ namespace LB4_IT
             {
                 for (int j = 0; j < 6; j += 2)
                 {
-                    Console.Write("{0}\t", matrix[i, j]);
+                    Console.Write("{0}\t", matrix[i, j]); // вывод всех столбцов, с которыми будут проводиться изменения
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
 
-            int count = 0;
+            int count = 0; // инициализируем счётчик, отвечающий за выполнение тела if
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 6; j +=2)
@@ -39,7 +40,7 @@ namespace LB4_IT
                         int mod = matrix[i, j] % (j + 1);
                         if (mod != 0)
                         {
-                            matrix[i, j] = mod;
+                            matrix[i, j] = mod; // приравнивание нужного элемента к остатку от целочисленного деления
                             count++;
                         }
                         Console.Write("{0}\t", matrix[i, j]);
